26ddef2ef6a433b109b028e1a8a3262e9ffaa1a8
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-12 22:53:44 +0900

    Implement web storage infrastructure

---
+++ packages/core/src/data/infrastructures/WebStorage.ts
@@ -0,0 +1,17 @@
+class WebStorage {
+  constructor(private readonly storage: Storage) {}
+
+  get(key: string): string | null {
+    return this.storage.getItem(key);
+  }
+
+  set(key: string, value: string): void {
+    this.storage.setItem(key, value);
+  }
+
+  remove(key: string): void {
+    this.storage.removeItem(key);
+  }
+}
+
+export default WebStorage;

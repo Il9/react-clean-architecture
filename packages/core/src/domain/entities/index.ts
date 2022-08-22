1931576c1c472dab11a6785ef386439d4cea038f
Myeongjin Choi <rntpwn90@gmail.com>
2022-08-22 21:02:00 +0900

    Remove useless exports

--- packages/core/src/domain/entities/index.ts
+++ packages/core/src/domain/entities/index.ts
@@ -1,5 +1,3 @@
 export { default as Task } from '@domain/entities/Task';
-export * from '@domain/entities/Task';
 
 export { default as Todo } from '@domain/entities/Todo';
-export * from '@domain/entities/Todo';
